5b7b33ad85e0ebdbbfad77de9d28ee72679e7967
Ismail143102 <nadeemladaf558@gmail.com>
2025-09-24 11:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-function getFormvalue() {
-    //Write your code here
-	event.preventDefault();
-	 const fname = document. queryselector('input[name="fname"]').value.trim();
-	const lname = document. queryselector('input[name="lname"]').value.trim();
-
-alert(fname + " " + lname);
-
-}
+function getFormvalue(event) {
+      event.preventDefault();
+      const fname = document.querySelector('input[name="fname"]').value.trim();
+      const lname = document.querySelector('input[name="lname"]').value.trim();
+      alert(fname + " " + lname);
+      return false;
+    }
